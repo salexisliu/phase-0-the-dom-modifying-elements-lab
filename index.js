683294ca6b6ad4d892ccf419fb6aeb86ccb86598
Alexis S Liu <syl1350@gmail.com>
2021-06-27 13:52:53 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Alexis is the champion"
